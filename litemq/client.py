43c8da2c7448dd42a0e771a3265710194220a138
thekashifmalik <emailkashifmalik@gmail.com>
2024-11-28 00:21:02 -0800

    Move python client into package

---
+++ litemq/client.py
@@ -0,0 +1,35 @@
+from grpclib.client import Channel
+
+from gen.service_pb2 import EnqueueRequest
+from gen.service_pb2 import QueueID
+from gen.service_pb2 import Nothing
+from gen.service_grpc import QueueServiceStub
+
+
+class LiteMQ:
+
+    def __init__(self, channel: Channel):
+        self.stub = QueueServiceStub(channel)
+
+    async def enqueue(self, queue: str, data: bytes):
+        request = EnqueueRequest(queue=queue, data=data)
+        response = await self.stub.Enqueue(request)
+        return response.count
+
+    async def dequeue(self, queue: str):
+        request = QueueID(queue=queue)
+        response = await self.stub.Dequeue(request)
+        return response.data
+
+    async def length(self, queue: str):
+        request = QueueID(queue=queue)
+        response = await self.stub.Length(request)
+        return response.count
+
+    async def purge(self, queue: str):
+        request = QueueID(queue=queue)
+        response = await self.stub.Purge(request)
+        return response.count
+
+    async def health(self):
+        return await self.stub.Health(Nothing())
--- main.py
+++ main.py
@@ -2,47 +2,17 @@ import asyncio
 import grpc
 from grpclib.client import Channel
 
-from gen.service_pb2 import EnqueueRequest
-from gen.service_pb2 import QueueID
-from gen.service_pb2 import Nothing
-from gen.service_grpc import QueueServiceStub
-
-
-class HTTPQ:
-    def __init__(self, channel: Channel):
-        self.stub = QueueServiceStub(channel)
-
-    async def enqueue(self, queue: str, data: bytes):
-        request = EnqueueRequest(queue=queue, data=data)
-        return await self.stub.Enqueue(request)
-
-    async def dequeue(self, queue: str):
-        request = QueueID(queue=queue)
-        response = await self.stub.Dequeue(request)
-        return response.data
-
-    async def length(self, queue: str):
-        request = QueueID(queue=queue)
-        response = await self.stub.Length(request)
-        return response.count
-
-    async def purge(self, queue: str):
-        request = QueueID(queue=queue)
-        response = await self.stub.Purge(request)
-        return response.count
-
-    async def health(self):
-        return await self.stub.Health(Nothing())
-
+from litemq.client import LiteMQ
 
 
 async def main():
     async with Channel('127.0.0.1', 42090) as channel:
-        client = HTTPQ(channel)
+        client = LiteMQ(channel)
         print('sending health')
         await client.health()
         print('sending enq')
-        await client.enqueue('hello', b'asdasd')
+        length = await client.enqueue('hello', b'asdasd')
+        print(length)
         print('sending deq')
         data = await client.dequeue('hello')
         print(data)
