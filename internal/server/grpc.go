9e487ee1c7c910195913b93782ec78f037a75156
thekashifmalik <emailkashifmalik@gmail.com>
2024-11-28 02:54:38 -0800

    Split out queue into it's own file

--- internal/server/grpc.go
+++ internal/server/grpc.go
@@ -16,55 +16,6 @@ type Server struct {
 	lock   sync.Mutex
 }
 
-type Queue struct {
-	messages [][]byte
-	channels []chan []byte
-	lock     sync.Mutex
-}
-
-func NewQueue() *Queue {
-	return &Queue{}
-}
-
-func (q *Queue) LockAndEnqueue(msg []byte) {
-	q.lock.Lock()
-	defer q.lock.Unlock()
-	if len(q.channels) > 0 {
-		channel := q.channels[0]
-		q.channels = q.channels[1:]
-		channel <- msg
-		close(channel)
-	} else {
-		q.messages = append(q.messages, msg)
-	}
-}
-
-func (q *Queue) LockAndDequeueOrChannel() ([]byte, chan []byte) {
-	q.lock.Lock()
-	defer q.lock.Unlock()
-	if len(q.messages) > 0 {
-		msg := q.messages[0]
-		q.messages = q.messages[1:]
-		return msg, nil
-	}
-	channel := make(chan []byte)
-	q.channels = append(q.channels, channel)
-	return nil, channel
-}
-
-func (q *Queue) LockAndDisconnect(channel chan []byte) {
-	q.lock.Lock()
-	defer q.lock.Unlock()
-	channels := []chan []byte{}
-	for _, ch := range q.channels {
-		if ch != channel {
-			channels = append(channels, ch)
-		}
-	}
-	q.channels = channels
-	close(channel)
-}
-
 func NewServer() *Server {
 	slog.Info("LiteMQ started")
 	return &Server{
