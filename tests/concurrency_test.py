5c8ba68119b51cd95c303aa05d24b764b34aecef
thekashifmalik <emailkashifmalik@gmail.com>
2025-08-07 13:53:59 -0700

    Fix race condition in test for blocking dequeue

--- tests/concurrency_test.py
+++ tests/concurrency_test.py
@@ -8,12 +8,13 @@ from .core import server, flushed_client
 async def test_enqueue_while_blocking_dequeue(server):
     client = await flushed_client()
 
-    async def task():
-        time.sleep(0.01)
+    async def enqueue_after_delay():
+        await asyncio.sleep(0.01)
         assert await client.enqueue('test-blocking', b'message') == 0
 
     # This test is flakey, as are all tests for concurrency. We run it a couple of time since it does reliably catch
     # errors.
     for _ in range(100):
-        asyncio.create_task(task())
+        task = asyncio.create_task(enqueue_after_delay())
         assert await client.dequeue('test-blocking') == b'message'
+        await task
